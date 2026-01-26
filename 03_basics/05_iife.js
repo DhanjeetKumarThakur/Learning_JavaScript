7397e7a1d640da7209e93403b005bcf14e06c628
Dhanjeet Kumar Thakur <68640018+DhanjeetKumarThakur@users.noreply.github.com>
2026-01-26 14:45:00 +0000

    IIFE

---
+++ 03_basics/05_iife.js
@@ -0,0 +1,10 @@
+//Immediately Invoke Function Expressions (IIFE)
+
+(function chai(){
+    console.log(`DB Connected...`);
+})();
+
+//Let's create an arrow function
+(() => {
+    console.log(`Hey Sonu`);
+})();
